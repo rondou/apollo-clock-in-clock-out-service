82fad525de34681eab9c20c54479add6e28bf531
Rondou Chen <40and44sis@gmail.com>
2022-05-13 16:56:18 +0800

    Dynamic get punch out time

--- apolloxe/apolloxe_api.py
+++ apolloxe/apolloxe_api.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from typing import Optional
 
 import asyncio
-import concurrent.futures
+import re
 
 
 class ApolloXeApi:
@@ -49,9 +49,18 @@ class ApolloXeApi:
         # button = self.chrome_driver.find_element(by=By.XPATH, value=f'//span[text()=\'{target}\']')
         button.click()
 
-    async def wait_check_in_done(self, time_out: int = 20) -> bool:
+    async def check_out_click(self, *, time_out: int = 20):
         wait = WebDriverWait(self.chrome_driver, timeout=time_out)
-        wait.until(EC.element_to_be_clickable((By.XPATH, f'//span[text()=\'下班\']')))
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, f'//div[@class=\'new-window-body clearfix\']//button[@class=\'ta_btn_cancel\']')))
+        print('-----')
+        print(button)
+        button.click()
+
+    async def wait_check_out_done(self, time_out: int = 20) -> bool:
+        result: bool = False
+
+        wait = WebDriverWait(self.chrome_driver, timeout=time_out)
+        wait.until(EC.element_to_be_clickable((By.XPATH, f'//span[text()=\'上班\']')))
         button = wait.until(EC.element_to_be_clickable((By.XPATH, f'//button[contains(@class, \'btn new-window-title-button\')]')))
         button.click()
 
@@ -59,7 +68,26 @@ class ApolloXeApi:
         await self.find_button_2_click('打卡紀錄')
         try:
             n: str = datetime.now().strftime('%Y/%m/%d')
-            wait.until(lambda d: d.find_element(by=By.XPATH, value=f'//td[text()=\'{n}\']'))
-            return True
+            # wait.until(lambda d: d.find_element(by=By.XPATH, value=f'//td[text()=\'{n}\']'))
+            xpath_string = f'//div[contains(@class, \'ta-scrollbar_wrapper ta_grid_table\')]//td[text()=\'{n}\']/following::td[3]'
+            check_out = wait.until(EC.visibility_of_element_located((By.XPATH, xpath_string)))
+            if check_out.text:
+                result = True
         except TimeoutException as e:
-            return False
+            result = False
+
+        return result
+
+    async def get_check_out_time(self) -> datetime:
+        wait = WebDriverWait(self.chrome_driver, timeout=20)
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, f'//button[contains(@class, \'btn new-window-title-button\')]')))
+        button.click()
+        await self.find_button_2_click('打卡紀錄')
+
+        n: str = datetime.now().strftime('%Y/%m/%d')
+        xpath_string = f'//div[contains(@class, \'ta-scrollbar_wrapper ta_grid_table\')]//td[text()=\'{n}\']/following-sibling::td'
+        check_in = wait.until(EC.visibility_of_element_located((By.XPATH, xpath_string)))
+        print(check_in.text)
+
+        m = re.match(r'^[0-9]{2}:[0-9]{2}(?=\/)', check_in.text)
+        return datetime.strptime(m.group(0), '%H:%M')
--- login_view.py
+++ login_view.py
@@ -1,5 +1,6 @@
 import asyncio
 import concurrent
+from datetime import datetime
 import time
 
 from apolloxe import ApolloXeApi
@@ -14,6 +15,8 @@ class PunchKeeper:
         self.loop = loop if loop else asyncio.get_running_loop()
         self.apollo_api = ApolloXeApi(self.loop)
         self.aio_sch = AsyncIOScheduler(timezone='Asia/Taipei')
+        self.dump_check_out_time_job = None
+        self.check_out_job = None
 
     async def check_task(self):
         print('Check task start')
@@ -23,19 +26,44 @@ class PunchKeeper:
         await self.apollo_api.login('', '')
         await self.apollo_api.find_button_2_click('登入')
         await self.apollo_api.find_button_2_click('我要打卡')
-        await self.apollo_api.find_button_2_click('上班')
-        return await self.apollo_api.wait_check_in_done()
+
+        # await self.apollo_api.find_button_2_click('上班')
+        await self.apollo_api.check_out_click()
+        return await self.apollo_api.wait_check_out_done()
+
+    async def dump_check_out_time_task(self):
+        print('Check task start')
+        await self.apollo_api.init_driver()
+
+        await self.apollo_api.get_view('')
+        await self.apollo_api.login('', '')
+        await self.apollo_api.find_button_2_click('登入')
+        await self.apollo_api.find_button_2_click('我要打卡')
+        return await self.apollo_api.get_check_out_time()
 
     def job_listener(self, event):
         if isinstance(event, JobExecutionEvent):
             asyncio.run_coroutine_threadsafe(self.apollo_api.destroy_driver(), self.loop)
 
-            print(event.retval)
             print(event.job_id)
-            if event.retval:
-                print('Success')
-            else:
-                print('Fails')
+            if event.job_id == self.dump_check_out_time_job.id:
+                print(f'The check in time is {event.retval}')
+                check_out_time: datetime = event.retval
+
+                hour, minute = check_out_time.hour + 9, check_out_time.minute
+                print(f'so the punch out time is {hour} : {minute}')
+
+                self.check_out_job.reschedule('cron', day_of_week='mon-fri', hour=hour, minute=minute)
+                self.check_out_job.resume()
+
+            if event.job_id == self.check_out_job.id:
+                self.check_out_job.pause()
+                punch_out_resutl: bool = event.retval
+
+                if punch_out_resutl:
+                    print('Success')
+                else:
+                    print('Fails')
 
     async def run(self):
         print('Prepare scheduler')
@@ -47,8 +75,15 @@ class PunchKeeper:
         # self.aio_sch.add_job(self.check_task, 'cron', day_of_week='tue-fri', hour=16, minute=7)
         # aio_sch.add_job(check_task, 'cron', day_of_week='wed-fri', hour=8, minute=33)
         # aio_sch.add_job(check_task, 'cron', day_of_week='thu-fri', hour=8, minute=30)
-        self.aio_sch.add_job(self.check_task, 'cron', day_of_week='tue-fri', hour=8, minute=35)
+        # self.aio_sch.add_job(self.check_task, 'cron', day_of_week='tue-fri', hour=8, minute=30)
+
+        self.dump_check_out_time_job = self.aio_sch.add_job(self.dump_check_out_time_task, 'cron', day_of_week='mon-fri', hour=17, minute=27)
         print(self.aio_sch.state)
+
+        # self.check_out_job = self.aio_sch.add_job(self.check_task, 'cron', day_of_week='mon-fri', hour=16, minute=45)
+        self.check_out_job = self.aio_sch.add_job(self.check_task)
+        self.check_out_job.pause()
+
         print(self.aio_sch.get_jobs())
 
         self.aio_sch.start()
